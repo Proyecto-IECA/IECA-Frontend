76643039dbcb70775d4dcdc4281e391d2ac0546e
Sarahinwn <riej98@gmail.com>
2021-02-24 20:19:18 -0600

    unir main con mainSarahi

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
+  styleUrls: [ '../../assets/pages/css/light-bootstrap-dashboard.css'
   ]
 })
 export class PagesComponent implements OnInit {
