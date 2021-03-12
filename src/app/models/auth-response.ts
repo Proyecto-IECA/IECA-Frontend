1ffa8eae539135a5ae868217008ecd6acdc17524
Sarahinwn <riej98@gmail.com>
2021-03-11 23:10:52 -0600

    Services CRUD  Curso, Exp Academica fixes #44,#45,#46,#32,#33,#34

--- src/app/models/auth-response.ts
+++ src/app/models/auth-response.ts
@@ -1,10 +1,14 @@
 import { UsuarioI } from './usuario';
 import { EmpresaI } from './empresa';
+import { ExperienciaLaboralI } from './experiencia_laboral';
+import { ExperienciaAcademicaI } from './experiencia_academica';
+import { CursoCertificacionI } from './cursos_certificaciones';
+
 
 export class AuthResponseI {
   status: boolean;
   message: string;
-  data: UsuarioI | EmpresaI;
+  data: any;
   token?: string;
   refreshToken?: string;
 }
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -7,6 +7,13 @@ export interface Habilidad{
   name: string;
 }
 
+export interface Valor{
+  name: string;
+}
+export interface Idioma{
+  name: string;
+}
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
@@ -23,11 +30,20 @@ export class UserProfileComponent implements OnInit {
 
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   habilidades: Habilidad[] = [
-    {name: 'Lemon'},
-    {name: 'Lime'},
-    {name: 'Apple'},
+    {name: 'Pensamiento Crítico'},
+    {name: 'Resolución de Problemas'}
   ];
 
+  valores: Valor[]= [
+    {name: 'Respeto'},
+    {name: 'Responsabilidad'},
+  ]
+
+  idiomas: Idioma[]= [
+    {name: 'Inglés'},
+    {name: 'Frances'}
+  ]
+  
   add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
@@ -37,17 +53,39 @@ export class UserProfileComponent implements OnInit {
       this.habilidades.push({name: value.trim()});
     }
 
+    if ((value || '').trim()) {
+      this.valores.push({name: value.trim()});
+    }
+    if ((value || '').trim()) {
+      this.idiomas.push({name: value.trim()});
+    }
     // Reset the input value
     if (input) {
       input.value = '';
     }
   }
 
-  remove(habilidad: Habilidad): void {
+  removeHab(habilidad: Habilidad): void {
     const index = this.habilidades.indexOf(habilidad);
 
     if (index >= 0) {
       this.habilidades.splice(index, 1);
+    }
+  }
+
+  removeVal(valor: Valor): void {
+    const index = this.valores.indexOf(valor);
+
+    if (index >= 0) {
+      this.valores.splice(index,1);
+    }
+  }
+
+  removeIdi(idioma: Idioma): void {
+    const index = this.idiomas.indexOf(idioma);
+
+    if (index >= 0) {
+      this.idiomas.splice(index,1);
     }
   }
   
