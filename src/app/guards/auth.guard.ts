022bc0af7f51c08bdc93a6b69909d2ae47dd1509
Eric Rangel Carmona <dreamereric28@gmail.com>
2021-03-11 23:55:32 -0600

    Carpeta Guards Recuperada

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanLoad {
+
+
+  constructor(private authService: AuthService,
+              private router: Router) {  }
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | boolean {
+    console.log('Guardian canActivate');
+    // return this.authService.validarToken()
+    return this.authService.validarEmailValidado()
+      .pipe(
+        tap((valid) => {
+          console.log(valid);
+          if (!valid) {
+            this.router.navigateByUrl('/validarEmail');
+          }
+          return true;
+        })
+      );
+  }
+
+  canLoad(): Observable<boolean> | boolean {
+    console.log('Guardian canLoad');
+    // return this.authService.validarEmailValidado()
+    return this.authService.validarToken()
+      .pipe(
+        tap((valid) => {
+          if (!valid) {
+            console.log(valid);
+            // this.router.navigateByUrl('/validarEmail');
+            this.router.navigateByUrl('/auth');
+          }
+          return true;
+        })
+      );
+  }
+
+}
