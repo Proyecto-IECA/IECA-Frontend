ab72a1bfa8969e60ee9bf530e5aa7075e687e4ad
Sarahinwn <riej98@gmail.com>
2021-03-24 17:33:21 -0600

    Chip autocomplete Idioma

--- src/app/pages/chips/idiomas/idiomas.component.ts
+++ src/app/pages/chips/idiomas/idiomas.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { AuthResponseI } from 'app/models/auth-response';
+import { IdiomaPostulanteI } from 'app/models/idioma_postulante';
 import { UsuarioService } from '../../../services/usuario.service';
 import { UsuarioI } from '../../../models/usuario';
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
-import { IdiomaPostulanteI } from 'app/models/idioma_postulante';
-import { MatChipInputEvent } from '@angular/material/chips';
-import { AuthResponseI } from 'app/models/auth-response';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { IdiomaI } from '../../../models/idioma';
+import { map, startWith } from 'rxjs/operators';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
 
 @Component({
   selector: 'app-idiomas',
@@ -14,16 +20,45 @@ import { AuthResponseI } from 'app/models/auth-response';
 export class IdiomasComponent implements OnInit {
 
   @Input() usuario: UsuarioI;
+  idiomas: IdiomaPostulanteI[];
+  idiomasAux: IdiomaPostulanteI[];
 
+  visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
+  guardarIdioma = false;
+  
+  idiomaCtrl = new FormControl();
+  filteredIdioma: Observable<IdiomaI[]>;
+  ListaIdiomas: IdiomaI[];
 
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  idiomas: IdiomaPostulanteI[];
-  idiomasAux: IdiomaPostulanteI[];
-  guardarIdioma = false;
+  @ViewChild('idiomaInput') idiomaInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') MatAutocomplete: MatAutocomplete;
+
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit(): void {
+    this.usuarioService.readIdiomasPostulante().subscribe((resp: AuthResponseI) => {
+      if(resp.status) {
+        this.idiomasAux = resp.data;
+      }
+    });
+
+    this.usuarioService.readIdiomas().subscribe((resp: AuthResponseI) => {
+      if(resp.status){
+        this.ListaIdiomas = resp.data;
+        this.filteredIdioma = this.idiomaCtrl.valueChanges.pipe(
+          startWith(null),
+          map((idioma: string | IdiomaI | null) => idioma ?
+          this._filter(idioma) : this.ListaIdiomas.slice()));
+      } 
+    })
+
+    this.idiomas = this.usuario.idiomas_postulante;
+  }
 
   addIdi(event: MatChipInputEvent): void {
     const input = event.input;
@@ -46,6 +81,8 @@ export class IdiomasComponent implements OnInit {
     } else {
       this.guardarIdioma = false;
     }
+
+    this.idiomaCtrl.setValue(null);
   }
 
   removeIdi(idioma: IdiomaPostulanteI): void {
@@ -61,19 +98,33 @@ export class IdiomasComponent implements OnInit {
       this.guardarIdioma = false;
     }
   }
-  
-  constructor(private usuarioService: UsuarioService) { }
 
-  ngOnInit(): void {
-    this.usuarioService.readIdiomasPostulante().subscribe((resp: AuthResponseI) => {
-      if(resp.status) {
-        this.idiomasAux = resp.data;
-      }
+  selectedIdi(event: MatAutocompleteSelectedEvent) : void{
+    this.idiomas.push({
+      id_postulante: this.usuario.id_postulante,
+      descripcion: event.option.viewValue
     });
+    this.idiomaInput.nativeElement.value = '';
+    this.idiomaCtrl.setValue(null);
 
-    this.idiomas = this.usuario.idiomas_postulante;
+    if (!this.compararArregos(this.idiomas, this.idiomasAux)) {
+      this.guardarIdioma = true;
+    } else {
+      this.guardarIdioma = false;
+    } 
   }
 
+
+  _filter(idioma: string | IdiomaI): IdiomaI[] {
+    let idiomaDescripcion = '';
+    if(typeof(idioma) == 'string') {
+      idiomaDescripcion = idioma.toLowerCase();
+    } else {
+      idiomaDescripcion = idioma.descripcion.toLowerCase();
+    }
+    return this.ListaIdiomas.filter(idioma => 
+      idioma.descripcion.toLowerCase().indexOf(idiomaDescripcion) === 0);
+  }
   guardarIdiomas() {
     this.usuarioService.createIdiomas(this.idiomas).subscribe((resp: AuthResponseI) => {
       if (resp.status) {
