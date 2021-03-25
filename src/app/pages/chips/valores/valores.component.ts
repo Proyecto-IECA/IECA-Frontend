b3f442c959aee1abfb1af999257124a4a17a5fcd
Sarahinwn <riej98@gmail.com>
2021-03-24 18:03:36 -0600

    Chip Autocomplete Valores

--- src/app/pages/chips/valores/valores.component.ts
+++ src/app/pages/chips/valores/valores.component.ts
@@ -1,10 +1,15 @@
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { AuthResponseI } from 'app/models/auth-response';
 import { ValorPostulanteI } from 'app/models/valor_postulante';
 import { UsuarioI } from '../../../models/usuario';
 import { UsuarioService } from '../../../services/usuario.service';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { ValorI } from 'app/models/valor';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-valores',
@@ -14,16 +19,45 @@ import { UsuarioService } from '../../../services/usuario.service';
 export class ValoresComponent implements OnInit {
 
   @Input() usuario: UsuarioI 
+  valores: ValorPostulanteI[];
+  valoresAux: ValorPostulanteI[];
 
+  visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
+  guardarValor = false;
+  
+  valorCtrl = new FormControl();
+  filteredValor: Observable<ValorI[]>
+  ListaValores: ValorI[];
 
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  valores: ValorPostulanteI[]= [];
-  valoresAux: ValorPostulanteI[];
-  guardarValor = false;
+  @ViewChild('valorInput') valorInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') MatAutocomplete: MatAutocomplete;
+  
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit(): void {
+    this.usuarioService.readValoresPostulante().subscribe((resp: AuthResponseI) => {
+      if (resp.status) {
+        this.valoresAux = resp.data;
+      }
+    });
+
+    this.usuarioService.readValores().subscribe((resp: AuthResponseI) => {
+      if(resp.status){
+        this.ListaValores = resp.data;
+        this.filteredValor = this.valorCtrl.valueChanges.pipe(
+          startWith(null),
+          map((valor: string | ValorI | null) => valor ?
+          this._filter(valor) : this.ListaValores.slice()));
+      }
+    })
+
+    this.valores = this.usuario.valores_postulante;
+  }
 
   addVal(event: MatChipInputEvent): void {
     const input = event.input;
@@ -63,16 +97,31 @@ export class ValoresComponent implements OnInit {
 
   }
 
-  constructor(private usuarioService: UsuarioService) { }
-
-  ngOnInit(): void {
-    this.usuarioService.readValoresPostulante().subscribe((resp: AuthResponseI) => {
-      if (resp.status) {
-        this.valoresAux = resp.data;
-      }
+  selectedVal(event: MatAutocompleteSelectedEvent): void{
+    this.valores.push({ 
+      id_postulante: this.usuario.id_postulante, 
+      descripcion: event.option.viewValue
     });
+    this.valorInput.nativeElement.value = '';
+    this.valorCtrl.setValue(null);
 
-    this.valores = this.usuario.valores_postulante;
+    if (!this.compararArregos(this.valores, this.valoresAux)) {
+      this.guardarValor = true;
+    } else {
+      this.guardarValor = false;
+    }
+
+  }
+
+  _filter(valor: string | ValorI): ValorI[] {
+    let valorDescripcion = '';
+    if(typeof(valor) == 'string') {
+      valorDescripcion = valor.toLowerCase();
+    } else {
+      valorDescripcion = valor.descripcion.toLowerCase();
+    }
+    return this.ListaValores.filter(valor => 
+      valor.descripcion.toLowerCase().indexOf(valorDescripcion) === 0);
   }
 
   guardarValores() {
