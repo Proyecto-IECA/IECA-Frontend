b636e258755c04b96385e9f98d5f39b896d7e646
Sarahinwn <riej98@gmail.com>
2021-04-20 19:06:43 -0500

    Models sucursales, models vacantes

---
+++ src/app/models/sucursales.ts
@@ -0,0 +1,5 @@
+export class SucursalesI {
+    id_sucursal: number;
+    ubicacion?: string;
+    nombre?: string;
+}
--- src/app/models/vacantes.ts
+++ src/app/models/vacantes.ts
@@ -2,9 +2,13 @@ export class VacantesI {
     id_vacante?: number;
     puesto?: string;
     fecha_publicacion?: string;
-    image?: string;
+    imagen?: string;
     sueldo?: string;
     descripcion?: string;
     disponible?: boolean;
+    modalidad: string;
+    nivel: string;
+    vistas: number;
+    publicada: number;
     id_empresa?: number;
 }
