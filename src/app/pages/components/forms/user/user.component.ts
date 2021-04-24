e371f15a143a812786b07464200c5be17657cfed
Sarahinwn <riej98@gmail.com>
2021-04-23 22:45:03 -0500

    Update Usuario service listo

--- src/app/pages/components/forms/user/user.component.ts
+++ src/app/pages/components/forms/user/user.component.ts
@@ -1,69 +1,71 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { UsuarioService } from '../../../../services/usuario.service';
-import { UsuarioI } from '../../../../models/usuario';
-import Swal from 'sweetalert2';
-import { AuthResponseI } from '../../../../models/auth-response';
-import * as moment from 'moment';
+import { Component, Input, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { UsuarioService } from "../../../../services/usuario.service";
+import { UsuarioI } from "../../../../models/usuario";
+import Swal from "sweetalert2";
+import { AuthResponseI } from "../../../../models/auth-response";
+import * as moment from "moment";
+import { UserService } from "./user.service";
 
-
-interface Sexo{
+interface Sexo {
   value: string;
   viewValue: string;
 }
 
-
 @Component({
-  selector: 'app-user-form',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user-form",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
-
   public selected;
   @Input() usuario: UsuarioI;
   public formSubmitted = false;
 
-  public userForm = this.formBuilder.group(
-    {
-    nombre: ['', Validators.required],
-    apellido_paterno: ['', Validators.required],
-    apellido_materno: ['', Validators.required],
-    sexo: ['', Validators.required],
-    fecha_nacimiento: ['', Validators.required],
-    telefono_casa: [''],
-    telefono_celular: [''],
-    codigo_postal: [''],
-    domicilio:[''],
-    ciudad:[''],
-    pais:['']
-   }
-  )
+  public userForm = this.formBuilder.group({
+    nombre: ["", Validators.required],
+    apellido_paterno: ["", Validators.required],
+    apellido_materno: ["", Validators.required],
+    sexo: ["", Validators.required],
+    fecha_nacimiento: ["", Validators.required],
+    telefono_casa: [""],
+    telefono: [""],
+    codigo_postal: [""],
+    domicilio: [""],
+    ciudad: [""],
+    pais: [""],
+  });
 
   sexo: Sexo[] = [
-    {value: 'H', viewValue: 'Hombre'},
-    {value: 'M', viewValue: 'Mujer'},
-    {value: 'X', viewValue: 'Prefiero no decirlo'}
-  ]
+    { value: "H", viewValue: "Hombre" },
+    { value: "M", viewValue: "Mujer" },
+    { value: "X", viewValue: "Prefiero no decirlo" },
+  ];
 
-  constructor(private formBuilder: FormBuilder, private usuarioService: UsuarioService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private usuarioService: UsuarioService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
-    this.userForm.reset(this.usuario);    
+    this.userForm.reset(this.usuario);
   }
 
   updateUser() {
     this.formSubmitted = true;
-    if(this.userForm.valid){
-      this.usuarioService.updateUsuario(this.userForm.value).subscribe((resp: AuthResponseI) => {
-        if(resp.status) {
-          this.doneMassage(resp.message);
-          this.userForm.reset(resp.data);
-          this.formSubmitted = false;
-        } else {
-          this.errorPeticion(resp.message);
-        }
-      }, (error) => this.errorServer(error));
+    if (this.userForm.valid) {
+      this.userService.updateUsuario(this.userForm.value).subscribe(
+        (resp: AuthResponseI) => {
+          if (resp.status) {
+            this.doneMassage("Información actualizada");
+            this.formSubmitted = false;
+          } else {
+            this.errorPeticion(resp.data);
+          }
+        },
+        (error) => this.errorServer(error)
+      );
     } else {
       this.errorMassage();
     }
@@ -75,7 +77,7 @@ export class UserComponent implements OnInit {
   }
 
   campoNoValido(campo: string): boolean {
-    if (this.userForm.get(campo).invalid && this.formSubmitted){
+    if (this.userForm.get(campo).invalid && this.formSubmitted) {
       return true;
     } else {
       return false;
@@ -83,10 +85,11 @@ export class UserComponent implements OnInit {
   }
 
   //  ---------- MENSAJES ---------- //
-  errorServer(error: any): void { // Lo sentimos su petición no puede ser procesada, favor de ponerse en contacto con soporte técnico
+  errorServer(error: any): void {
+    // Lo sentimos su petición no puede ser procesada, favor de ponerse en contacto con soporte técnico
     Swal.fire({
-      icon: 'error',
-      title: 'Petición NO procesada',
+      icon: "error",
+      title: "Petición NO procesada",
       text: `Vuelve a intentar de nuevo...
       Si el error persiste ponerse en contacto con soporte técnico`,
     });
@@ -95,33 +98,31 @@ export class UserComponent implements OnInit {
 
   errorMassage(): void {
     Swal.fire({
-      icon: 'error',
-      title: 'Revisa el formulario',
-      text: 'Revisa que el formulario esté correctamente llenado',
+      icon: "error",
+      title: "Revisa el formulario",
+      text: "Revisa que el formulario esté correctamente llenado",
       showConfirmButton: false,
-      timer: 2700
+      timer: 2700,
     });
   }
 
   doneMassage(message: string): void {
     Swal.fire({
-      icon: 'success',
-      title: 'Cambios Actualizados',
+      icon: "success",
+      title: "Cambios Actualizados",
       text: message,
       showConfirmButton: false,
-      timer: 2700
+      timer: 2700,
     });
   }
 
   errorPeticion(error: string): void {
     Swal.fire({
-      icon: 'error',
-      title: 'Error',
+      icon: "error",
+      title: "Error",
       text: error,
       showConfirmButton: false,
-      timer: 2700
+      timer: 2700,
     });
   }
-
-
 }
