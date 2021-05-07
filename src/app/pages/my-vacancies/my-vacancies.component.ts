622f0dc19577e3ad72241763149729e73d45a63b
GibbyStack <carlosorozco4565@gmail.com>
2021-05-07 16:55:32 -0500

    Lista de vacantes con filtro fixes #67

--- src/app/pages/my-vacancies/my-vacancies.component.ts
+++ src/app/pages/my-vacancies/my-vacancies.component.ts
@@ -3,6 +3,7 @@ import { VacantesI } from '../../models/vacantes';
 import { MyVacanciesService } from './my-vacancies.service';
 import { AuthResponseI } from '../../models/auth-response';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-my-vacancies',
@@ -45,4 +46,65 @@ export class MyVacanciesComponent implements OnInit {
     this.router.navigate(['/update-vacancie', id]);
   }
 
+  publicar(vacante) {
+    this.myVacanciesService.publicarVacante(vacante.id_vacante).subscribe((resp: AuthResponseI) => {
+      if (resp.status) {
+        this.doneMassage("Vacante públicada");
+        let index = this.vacantesB.indexOf(vacante);
+        let vacanteP = this.vacantesB.splice(index, 1);
+        this.vacantesP.unshift(vacanteP[0]);
+        this.numVacantesB = this.numVacantesB - 1;
+        this.numVacantesP = this.numVacantesP + 1;
+      }
+    })
+  }
+
+  noPublicar(vacante) {
+    this.myVacanciesService.noPublicarVacante(vacante.id_vacante).subscribe((resp: AuthResponseI) => {
+      if (resp.status) {
+        this.doneMassage("Vacante no públicada");
+        let index = this.vacantesP.indexOf(vacante);
+        let vacanteB = this.vacantesP.splice(index, 1);
+        this.vacantesB.unshift(vacanteB[0]);
+        this.numVacantesP = this.numVacantesP - 1;
+        this.numVacantesB = this.numVacantesB + 1;
+      }
+    })
+  }
+
+  cerrar(vacante) {
+    this.myVacanciesService.cerrarVacante(vacante.id_vacante).subscribe((resp: AuthResponseI) => {
+      if (resp.status) {
+        this.doneMassage("Vacante cerrada");
+        let index = this.vacantesD.indexOf(vacante);
+        let vacanteC = this.vacantesD.splice(index, 1);
+        this.vacantesC.unshift(vacanteC[0]);
+        this.numVacantesD = this.numVacantesD - 1;
+        this.numVacantesC = this.numVacantesC + 1;
+      }
+    })
+  }
+
+  abrir(vacante) {
+    this.myVacanciesService.abrirVacante(vacante.id_vacante).subscribe((resp: AuthResponseI) => {
+      if (resp.status) {
+        this.doneMassage("Vacante abierta");
+        let index = this.vacantesC.indexOf(vacante);
+        let vacanteD = this.vacantesC.splice(index, 1);
+        this.vacantesD.unshift(vacanteD[0]);
+        this.numVacantesC = this.numVacantesC - 1;
+        this.numVacantesD = this.numVacantesD + 1;
+      }
+    })
+  }
+  
+  doneMassage(message: string): void {
+    Swal.fire({
+      icon: 'success',
+      title: 'Cambios Actualizados',
+      text: message,
+      showConfirmButton: false,
+      timer: 2700
+    });
+  }
 }
