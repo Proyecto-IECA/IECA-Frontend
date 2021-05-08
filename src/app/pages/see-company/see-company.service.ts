02bedd94a599c2df5194b11bac92cdec13bdbef1
Eric Rangel Carmona <dreamereric28@gmail.com>
2021-05-07 19:47:59 -0500

    Usuario - Perfil Empresa fixes #66

--- src/app/pages/see-company/see-company.service.ts
+++ src/app/pages/see-company/see-company.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+const baseUrl = environment.baseUrl + '/comentarios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeeCompanyService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getCompany(id: number): Observable<any> {
+    return this.http.get(`${baseUrl}/empresa/${id}`)
+  }
+
 }
